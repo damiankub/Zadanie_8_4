83e66d150a9e49fb1fd1afe196524b6efeda9f17
Damian <damiankub89@gmail.com>
2017-09-25 20:06:36 +0200

    Komentarze

---
+++ scripts.js
@@ -0,0 +1,7 @@
+// Wpisz swoje imie
+var name = prompt('Enter your name');
+/* alert okienko z wpisanym imieniem
+console wpis w konsoli
+ */
+alert('Hello, ' + name);
+console.log('Hello, ' + name);
